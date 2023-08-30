d91b9639c4afee7380eb01224e765648f8210948
yn197 <1607401968@qq.com>
2023-08-30 15:22:17 +0800

    cicd修改

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,12 @@
-#! bin/bash
-# 外部传给内部的变量脚本
-app_name="ruoyi_plus"
-port="80"
-docker_username="16074yang"
-
-echo "对容器存活进行判断"
-if test -n "$(docker ps -a |grep $app_name)" ; then
-  echo "停止并且删除容器和上版本镜像"
-  docker stop $app_name
-  docker rm $app_name
-  docker rmi $docker_username/$app_name:latest
-else
-  echo "未检查到$app_name容器运行"
-fi
-
-echo "获取最新的镜像"
-docker pull $docker_username/$app_name:latest
-echo "启动服务"
-# 有需要添加的环境变量随时添加
-docker run -itd\
- -e TZ="Asia/Shanghai"\
- -e SERVER_PORT=$port\
- -p $port:$port\
- --name $app_name  $docker_username/$app_name:latest
+# run.sh
+# 切换到jar包目录下
+cd /root/GitHub_Action
+# 杀死之前的项目进程。这里的cut -c9-14是截取进程id，不一定都是-c9-14
+# 可以先运行ps -ef|grep java|grep GitHub_Action-0.0.1-SNAPSHOT.jar看一下进程id是第几位到第几位
+ps -ef|grep java|grep ruoyi-admin.jar|cut -c9-14|xargs kill -9
+# 停5秒
+sleep 5s
+# 使环境变量生效
+source /root/.bash_profile
+# 运行项目
+nohup java -jar ruoyi-admin.jar >/root/cicd.log 2>&1 &
